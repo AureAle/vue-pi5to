3945a5fe4c3ee77ed1cd149bec166bb1ee448847
alexzaizar09 <alexzaizar09@gmail.com>
2018-11-15 22:37:33 -0600

    WIP /usuarios

--- src/views/Usuarios.vue
+++ src/views/Usuarios.vue
@@ -1,33 +1,90 @@
 <template>
-  <form action="https://java-rest-server.herokuapp.com/usuarios/" method="post">
-    <label for="name">Nombre:</label>
-    <input type="text" name="name" >
+  <section>
+    <form action="#" method="post">
+      <label for="name">Nombre:</label>
+      <input type="text" name="name" v-model="name" >
+      <br>
+      <label for="name">Career:</label>
+      <input type="text" name="career" v-model="career" >
+      <br>
+      <label for="name">Email:</label>
+      <input type="text" name="email" v-model="email" >
+      <br>
+      <label for="name">User:</label>
+      <input type="text" name="user" v-model="user" >
+      <br>
+      <label for="name">Password:</label>
+      <input type="password" name="password" v-model="password" >
+      <br>
+      <label for="name">Cuenta:</label>
+      <input type="number" name="cuenta" v-model="cuenta" >
+      <br>
+      <label for="name">Type:</label>
+      <input type="text" name="type" v-model="type" >
+      <br>
+      <button type="submit" @click.prevent="sendData">Registrar</button>
+    </form>
     <br>
-    <label for="name">Career:</label>
-    <input type="text" name="career" >
-    <br>
-    <label for="name">Email:</label>
-    <input type="text" name="email" >
-    <br>
-    <label for="name">User:</label>
-    <input type="text" name="user" >
-    <br>
-    <label for="name">Password:</label>
-    <input type="text" name="password" >
-    <br>
-    <label for="name">Cuenta:</label>
-    <input type="number" name="cuenta" >
-    <br>
-    <label for="name">Type:</label>
-    <input type="text" name="type" >
-    <br>
-    <button type="submit">Registrar</button>
-  </form>
+    <div v-if="success">
+      <p>Usuario registrado correctamente!</p>
+      <router-link to="/">Home</router-link>
+    </div>
+  </section>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "usuarios"
+  name: "usuarios",
+  data() {
+    return {
+      name: null,
+      career: null,
+      email: null,
+      user: null,
+      password: null,
+      cuenta: null,
+      type: null,
+      success: false,
+      error: false
+    };
+  },
+  methods: {
+    sendData() {
+      console.log("here");
+      axios({
+        method: "POST",
+        url: "https://java-rest-server.herokuapp.com/usuarios/",
+        data: {
+          name: this.name,
+          career: this.career,
+          email: this.email,
+          user: this.user,
+          password: this.password,
+          cuenta: this.cuenta,
+          type: this.typel
+        },
+        headers: { "content-type": "application/json" }
+      }).then(
+        result => {
+          if (result.data.id > 0) {
+            this.success = true;
+            this.name = null;
+            this.career = null;
+            this.email = null;
+            this.user = null;
+            this.password = null;
+            this.cuenta = null;
+            this.type = null;
+          }
+        },
+        error => {
+          this.error = true;
+        }
+      );
+    }
+  }
 };
 </script>
 
